7e7878186ba981e1e9b83b18f8268f180b275b42
Saravanan-Rajasekharan <saravanan.rajasekharan@gmail.com>
2023-03-01 21:59:18 +0530

    Response DTOs

--- src/main/java/com/example/Student_Library_Management_System/Service/AuthorService.java
+++ src/main/java/com/example/Student_Library_Management_System/Service/AuthorService.java
@@ -1,10 +1,16 @@
 package com.example.Student_Library_Management_System.Service;
 
 import com.example.Student_Library_Management_System.DTOs.AuthorEntryDto;
+import com.example.Student_Library_Management_System.DTOs.AuthorResponseDto;
+import com.example.Student_Library_Management_System.DTOs.BookResponseDto;
 import com.example.Student_Library_Management_System.Models.Author;
+import com.example.Student_Library_Management_System.Models.Book;
 import com.example.Student_Library_Management_System.Repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AuthorService {
@@ -30,6 +36,37 @@ public class AuthorService {
 
         authorRepository.save(author);
         return "Author added successfully";
+    }
+
+    public AuthorResponseDto getAuthor(Integer authorId){
+        Author author = authorRepository.findById(authorId).get();
+        AuthorResponseDto authorResponseDto = new AuthorResponseDto();
+        //Set its attributes
+
+        //List of book --->List<BookResponseDto>
+
+        List<Book> bookList = author.getBooksWritten();
+
+        List<BookResponseDto> booksWrittenDto = new ArrayList<>();
+        for(Book b : bookList){
+            BookResponseDto bookResponseDto = new BookResponseDto();
+            bookResponseDto.setName(b.getName());
+            bookResponseDto.setPages(b.getPages());
+            bookResponseDto.setGenre(b.getGenre());
+
+            booksWrittenDto.add(bookResponseDto);
+        }
+
+        //Set attributes in authorResponseDto
+
+        authorResponseDto.setBooksWritten(booksWrittenDto);
+        authorResponseDto.setName(author.getName());
+        authorResponseDto.setAge(author.getAge());
+        authorResponseDto.setCountry(author.getCountry());
+        authorResponseDto.setRating(author.getRating());
+
+
+        return authorResponseDto;
     }
 
 }
